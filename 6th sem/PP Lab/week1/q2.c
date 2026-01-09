20d932b52683dd777385d3d332f51c18a3a73133
npshetty25 <nirav.mitmpl2023@learner.manipal.edu>
2026-01-09 22:35:35 +0530

    Add file reversal and size display functionality
    This program reverses the contents of a specified file and writes the reversed content to another file. It also displays the size of the original file.

---
+++ 6th sem/PP Lab/week1/q2.c
@@ -0,0 +1,33 @@
+//2.To reverse the file contents and store in another file. Also display the size of file using file handling function.
+#include <stdio.h>
+
+void main()
+{
+	FILE * fptr1, * fptr2;
+	char fileName1[100], fileName2[100];
+	printf("Enter filename to open for reading ");
+	scanf("%s", fileName1);
+	printf("Enter filename to open for writing ");
+	scanf("%s", fileName2);
+	fptr1 = fopen(fileName1, "r");
+	if (fptr1 != NULL) {
+		fptr2 = fopen(fileName2, "w+");
+		char ch;
+		int i = -1;
+		while (ftell(fptr1) != 1)
+		{
+			fseek(fptr1, i--, SEEK_END);
+			ch = fgetc(fptr1);
+			fputc(ch, fptr2);
+		}
+		printf("%s contains reverse of %s\n", fileName2, fileName1);
+		fseek(fptr1, 0, SEEK_END);
+		printf("Size of %s: %ld bytes\n", fileName1, ftell(fptr1));
+		fclose(fptr1);
+		fclose(fptr2);
+	}
+	else
+	{
+		printf("Could not open file: %s", fileName1);
+	}
+}
