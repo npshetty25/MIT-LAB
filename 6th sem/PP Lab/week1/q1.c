b2a01cd522cd98baa28e49463caf0940cc764ae1
npshetty25 <nirav.mitmpl2023@learner.manipal.edu>
2026-01-09 22:38:39 +0530

    Implement MPI program for power calculation and messaging
    This program uses MPI to compute the power of a user-defined integer for each process's rank and prints messages based on the rank's parity.

--- 6th sem/PP Lab/week1/q1.c
+++ 6th sem/PP Lab/week1/q1.c
@@ -1,25 +1,35 @@
+//1.Write a simple MPI program to find out pow (x, rank) for all the processes where 'x' is the integer constant and 'rank' is the rank of the process. Write a program in MPI where even ranked process prints "Hello" and odd ranked process prints "World".
+#include <mpi.h>
 #include <stdio.h>
+#include <math.h>
 
-void main() {
-	FILE * fptr;
-	char fileName[100];
-	printf("Enter filename to open for reading : ");
-	scanf("%s", fileName);
-	fptr = fopen(fileName, "r");
-	if (fptr != NULL)
-    {
-		int charCount = 0, lineCount = 0;
-		char ch = fgetc(fptr);
-		while(ch != EOF) {
-			if (ch == '\n')
-				lineCount++;
-			else
-				charCount++;
-			ch = fgetc(fptr);
-		}
-		fclose(fptr);
-		printf("Number of lines: %d\nNumber of characters: %d\n", lineCount + 1, charCount);
-	}
-	else 
-		printf("Could not open file: %s\n", fileName);
+int main(int argc, char *argv[])
+{
+    int rank, size;
+    int x;   // user input
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    /* Take input only from rank 0 */
+    if (rank == 0) {
+        printf("Enter an integer value for x: ");
+        scanf("%d", &x);
+    }
+
+    /* Broadcast x to all processes */
+    MPI_Bcast(&x, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    double result = pow(x, rank);
+
+    printf("Process %d: %d^%d = %.0f\n", rank, x, rank, result);
+
+    if (rank % 2 == 0)
+        printf("Process %d says: Hello\n", rank);
+    else
+        printf("Process %d says: World\n", rank);
+
+    MPI_Finalize();
+    return 0;
 }
