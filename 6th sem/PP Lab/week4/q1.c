0c5f497e9a5d2a5c91fbbdc1c7fa1276c89c65f7
npshetty25 <nirav.mitmpl2023@learner.manipal.edu>
2026-01-30 15:53:22 +0530

    Create q1.c

---
+++ 6th sem/PP Lab/week4/q1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <mpi.h>
+int factorial(int n) 
+{
+    int f = 1;
+    for(int i = 1; i <= n; i++)
+    f *= i;
+    return f;
+}
+int main(int argc, char *argv[])
+{
+    int rank, size;
+    int fact, result;
+    int err;
+    MPI_Init(&argc, &argv);
+    MPI_Errhandler_set(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    fact = factorial(rank + 1);
+    err = MPI_Reduce(&fact, &result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if(err != MPI_SUCCESS)
+    {
+        char errstr[MPI_MAX_ERROR_STRING];
+        int len;
+        MPI_Error_string(err, errstr, &len);
+        printf("MPI Error: %s\n", errstr);
+    }
+    if(rank == 0) 
+    {
+        printf("Sum of factorials = %d\n", result);
+    }
+    MPI_Finalize();
+    return 0;
+}
