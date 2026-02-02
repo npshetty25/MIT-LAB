5b22cbc89abeafc553dc5dc249f17f7e2ecbe0a3
npshetty25 <nirav.mitmpl2023@learner.manipal.edu>
2026-02-02 13:53:04 +0530

    Add input.c with a simple addition function

---
+++ 6th sem/CD LAB/week4/input.c
@@ -0,0 +1,18 @@
+#define MAX_VALUE 100
+
+/*
+this is a 
+multiline comment
+*/
+
+int func (int a, int b) {
+    return a + b;
+}
+
+void main(int argc, char *argv[])
+{
+int a,b, sum , arr[1];
+a = 1; // this is a single line comment
+b = 1;
+sum = a + b;
+}
