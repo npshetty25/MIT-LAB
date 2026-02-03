d6c91692962e0a9b69f2cf5db2ad15e79a24f674
npshetty25 <nirav.mitmpl2023@learner.manipal.edu>
2026-02-03 10:50:38 +0530

    Update input.c

--- 6th sem/CD LAB/week4/input.c
+++ 6th sem/CD LAB/week4/input.c
@@ -1,18 +1,30 @@
-#define MAX_VALUE 100
-
-/*
-this is a 
-multiline comment
-*/
-
-int func (int a, int b) {
-    return a + b;
-}
-
-void main(int argc, char *argv[])
+#include<stdio.h>
+int main(int argc, char const *argv[])
 {
-int a,b, sum , arr[1];
-a = 1; // this is a single line comment
-b = 1;
-sum = a + b;
+    int a =10;
+    float b=3.5;
+    printf("value of a is %d\n",a);
+    printf("value of b is %.2f\n",b);
+    //arithmetic operation
+    int sum=a+b;
+    int diff=a-b;
+    int prod=a*b;
+    float div=a/b;
+
+    if (a>b && b!=0)
+    {
+        printf("a is greater than b and b is not zero\n");
+    }
+    else if (a==b || b==0)
+    {
+        printf("a is equal to b or b is zero\n");
+    }
+    else
+    {
+        printf("a is less than b and b is not zero\n");
+    }
+    
+
+    return 0;
 }
+
