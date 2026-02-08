5f5d8f4d0faba383f5933b49d420975a35f1563c
npshetty25 <nirav.mitmpl2023@learner.manipal.edu>
2026-02-09 02:02:37 +0530

    Add files via upload

---
+++ 6th sem/CD LAB/week5/TextForQ4Q5Q6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() {
+    // THIS IS a COMMENT
+    /*
+        THIS IS a COMMENT
+    */
+    int a, b, c = 20, D = 89, eVARIABLE = 10;
+    printf("Enter two numbers ");
+    scanf("%d %d", &a, &b);
+    printf("Sum is: %d\n", (a + b));
+}
